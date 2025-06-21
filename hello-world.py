831d2fd024b2da4f0c31d06b4efeef627f045b85
Marco Silva <msnet02@gmail.com>
2025-06-21 15:08:27 +0000

    Configuración de credential.helper en Ubuntu

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,4 @@
 # Mi primer programa en Python
 # Actualización: 21/06/2025
-# Se configuró credential.helper: Validado
+# Se configuró credential.helper: Validado/Fedora
 print("Hello World: I'm working from Fedora and Ubuntu/WSL\n")
