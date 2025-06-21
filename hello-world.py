56dc163da1946ebf676f0fe28247c4a5d6571f1f
Marco Silva <msnet02@gmail.com>
2025-06-21 15:10:19 +0000

    Validación de credential.helper en Fedora y Ubuntu

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,4 @@
 # Mi primer programa en Python
 # Actualización: 21/06/2025
-# Se configuró credential.helper: Validado/Fedora
+# Se configuró credential.helper: Validado/Fedora - Validado/Ubuntu
 print("Hello World: I'm working from Fedora and Ubuntu/WSL\n")
