3fbf2f28d24ec1ed56dea97123385385fe8f7575
Marco Silva <msnet02@gmail.com>
2025-07-09 20:47:46 -0300

    Se agregó el ejercicio con el operador ternario.

---
+++ code/ternary.py
@@ -0,0 +1,16 @@
+# Programa: ternary.py
+# Actualización: 09/07/2025
+
+# Main code
+order_total = 100
+
+# Classic if/else form
+if order_total > 100:
+	discount = 25
+else:
+	discount = 0
+print(f"Orden total: {order_total} | Descuento: {discount}")
+
+# Ternary operator
+discount = 25 if order_total > 100 else 0
+print(f"Orden total: {order_total} | Descuento: {discount}")
