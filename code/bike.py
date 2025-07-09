dd2ed7677a3c342a5f24bc4ff4fb3f96213f8cbd
Marco Silva <msnet02@gmail.com>
2025-07-09 09:54:00 -0300

    Se añadió el atributo 'name' y el método browse() a la clase Bike, en el programa code/bike.py

--- code/bike.py
+++ code/bike.py
@@ -1,28 +1,30 @@
 # Let's define the class Bike
 class Bike:
-    def __init__(self, colour, frame_material):
+    def __init__(self, name, colour, frame_material):
+        self.name = name
         self.colour = colour
         self.frame_material = frame_material
 
+    def browse(self):
+        print(f"{self.name}: {self.colour} | {self.frame_material}")
+
     def drive(self):
-        print(f"Bike: {self.colour} | {self.frame_material} - Driving!")
+        print(f"{self.name} - Driving!")
 
     def brake(self):
-        print(f"Bike: {self.colour} | {self.frame_material} - Braking!")
+        print(f"{self.name} - Driving!")
 
 # Main code
 # Let's create a couple of intances
-bike_one = Bike('Red', 'Carbon fiber')
-bike_two = Bike('Blue', 'Steel')
+bike_one = Bike('Lamborgini TX-100', 'Red', 'Carbon fiber')
+bike_two = Bike('Ferrari Z-1000x', 'Blue', 'Steel')
 
 # Let's inspect the objects we have, instances of the Bike class
-print(bike_one.colour) # prints: Red
-print(bike_one.frame_material) # prints: Carbon fiber
-print(bike_two.colour) # prints: Blue
-print(bike_two.frame_material) # prints: Steel
+bike_one.browse()
+bike_two.browse()
 
 # Let's brake!
-bike_one.brake() # prints: Bike: Colour | Frame material - Braking!
+bike_one.brake() # prints: Bike name - Braking!
 
 # Let's drive!
-bike_two.drive() # prints: Bike: Colour | Frame material - Driving!
+bike_two.drive() # prints: Bike name - Driving!
