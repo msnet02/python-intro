c42434880f9aa0aec7907223b344a684b9c398ae
Marco Silva <msnet02@gmail.com>
2025-07-11 13:00:39 -0300

    Se agregan los dos primeros ejercicios usando el bucle 'for'.

---
+++ code/simple-for-1.py
@@ -0,0 +1,23 @@
+# Programa: simple-for-1.py
+# Creación: 11/07/2025
+
+# Let's create the show_message() funtion
+def show_message(text):
+	print(f"{'-' * 3}\n{text}:")
+
+# Main code
+
+# Using a list of integer items
+show_message('Using a list of integer items')
+for number in [0, 1, 2, 3, 4]:
+	print(number)
+
+# Using built-it function range()
+show_message('# Using built-it function range()')
+for number in range(5):
+	print(number)
+
+# Using built-it function range(-10, 10, 4)
+show_message('Using built-it function range(-10, 10, 4)')
+for number in range(-10, 10, 4):
+	print(number)
---
+++ code/simple-for-2.py
@@ -0,0 +1,14 @@
+# Programa: simple-for-2.py
+# Creación: 11/07/2025
+
+# Let's create the show_message() funtion
+def show_message(text):
+	print(f"{'-' * 3}\n{text}:")
+
+# Main code
+
+# Using built-it function range(len(surnames))
+show_message('Using built-it function range(len(surnames))')
+surnames = ['Rivest', 'Shamir', 'Adleman']
+for position in range(len(surnames)):
+	print(position, surnames[position])
