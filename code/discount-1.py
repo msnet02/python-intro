b022e329aef8f9b9d8cfed9ae78ef132412f57fb
Marco Silva <msnet02@gmail.com>
2025-07-12 22:35:32 -0300

    Se replanteó el código en el programa discount-1.py; conviene mejorarlo en una futura versión.

--- code/discount-1.py
+++ code/discount-1.py
@@ -1,14 +1,13 @@
 # Programa: discount-1.py
 # Creación: 12/07/2025
+# Observación: Este código debe ser mejorado
 
 from datetime import date, timedelta
 
-def show_separator():
-    print(f"{'-' * 5}")
-
-def show_current_prices(list_products):
+def show_current_prices(list_products, message):
+    print(f"{'-' * 3} {message} {'-' * 3}") # Prints a special title
     for product in list_products:
-        print(f"Official price for sku {product['sku']} is {product['price']}")
+        print(f"Price for id_prod {product['id_prod']} is {product['price']}")
 
 # Main code
 today = date.today()
@@ -16,18 +15,19 @@ tomorrow = today + timedelta(days=1) # today + 1 day is tomorrow
 
 # Data: list of dicts
 products = [
-    {'sku': '1', 'expiration_date': today, 'price': 100.0},
-    {'sku': '2', 'expiration_date': tomorrow, 'price': 50.0},
-    {'sku': '3', 'expiration_date': today, 'price': 20.0},
+    {'id_prod': '1', 'expiration_date': today, 'price': 100.0},
+    {'id_prod': '2', 'expiration_date': tomorrow, 'price': 50.0},
+    {'id_prod': '3', 'expiration_date': today, 'price': 20.0},
 ]
 
 # Showing current prices
-show_current_prices(products)
+show_current_prices(products, 'Original price')
 
 # Applying discount for today
-show_separator()
 for product in products:
     if product['expiration_date'] != today:
         continue # No applying any discounts: This stops current iteration
     product['price'] *= 0.8 # equivalent to applying 20% discount
-    print(f"Price for sku {product['sku']} is now {product['price']}")
+
+# Showing current prices with some discounts applyied
+show_current_prices(products, 'With some discounts applied')
