f4a2fb428694b901d803e233ed0f67be21cddb0d
Marco Silva <msnet02@gmail.com>
2025-07-24 05:34:34 -0300

    Se ha agregado el ejercicio error-alert-2.py para mostrar el uso de la estructura de decisión match/case.

--- code/error-alert-2.py
+++ code/error-alert-2.py
@@ -1,5 +1,5 @@
-# Programa: error-alert.py 
-# Creación: 09/07/2025
+# Programa: error-alert-2.py
+# Creación: 24/07/2025
 
 def send_email(email_address, error_message):
     """Función que simula el envío de un mensaje por Email"""
@@ -7,15 +7,16 @@ def send_email(email_address, error_message):
 
 # Main code
 alert_system = 'email' # Possible values: 'console' or 'email' 
-error_severity = 'low' # Possible values: 'critical, 'medium' or 'low'
+error_severity = 'medium' # Possible values: 'critical, 'medium' or 'low'
 error_message = 'OMG! Something terrible happened'
 
 if alert_system == 'console':
     print(error_message)
 elif alert_system == 'email':
-    if error_severity == 'critical':
-        send_email('admin@example.com', error_message)
-    elif error_severity == 'medium':
-        send_email('support.1@example.com', error_message)
-    else:
-        send_email('support.2@example.com', error_message)
+    match error_severity:
+        case 'critical':
+            send_email('admin@example.com', error_message)
+        case 'medium':
+            send_email('support.1@example.com', error_message)
+        case _:
+            send_email('support.2@example.com', error_message)
